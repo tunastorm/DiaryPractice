2f7285776ca5bc037769237f9da5d4cc268e43fb
tunastorm <ycw921@naver.com>
2024-05-19 20:28:34 +0900

    Update: LaunchScreen 수정

---
+++ DiaryPractice/Contoller/LaunchScreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LaunchScreenViewController.swift
+//  DiaryPractice
+//
+//  Created by 유철원 on 5/19/24.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sleep(8)
+     
+    }
+    
+
+ 
+
+}
